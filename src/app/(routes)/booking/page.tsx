81188a003e8b1a238f801ad433044125fb1e8967
Chidanandan P <chidutramp@gmail.com>
2025-03-21 17:31:42 +0000

    installing framer motion package

--- src/app/(routes)/booking/page.tsx
+++ src/app/(routes)/booking/page.tsx
@@ -1,8 +1,21 @@
 
+"use client";
+
+import { motion } from 'framer-motion';
 import React from 'react';
 
 const Booking = () => {
-    return <h1>Booking</h1>
+    return (
+        <>
+            <main>
+                <motion.h1
+                initial={{opacity: 0, y: -50}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 1}}
+                >Welcome to booking</motion.h1>
+            </main>
+        </>
+    )
 }
 
 export default Booking;
